b3294b8e20de57dd1ab2d63ed4a1605b8a284ffa
georgeadolphus <george@zambaapp.com>
2016-08-26 18:24:59 +0100

    Edit text - at a time

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <h2>This is the testing file, where I will try to get good at development from</h2>
 <p>I have a lot to learn but I am certain that I will get there</p>
 <p>I can't wait</p>
-<p>When I have done this I will rule the world, one line of code at a </p>
+<p>When I have done this I will rule the world, one line of code at a time</p>
